b2ca3e1d2312cd1a84805ee9ed4202499ca81d8a
pourya ghazizadeh <pourya.gh095@gmail.com>
2021-09-25 12:57:37 +0200

    enum tupe for app

--- src/components/API.ts
+++ src/components/API.ts
@@ -1,5 +1,17 @@
+import { queries } from "@testing-library/dom"
+import { type } from "os"
+
+import { shuffleArray } from "./utils"
+export type Question={
+    category:string;
+    correct_answers:string;
+    incorrect_answers:string[];
+    question:string;
+    type:string 
 
 
+}
+export type QuestionState = Question & {answer:string[ ]}
 export  enum Difficulty {
     EASY="easy",
     MEDUM="medum",
@@ -11,6 +23,11 @@ export  enum Difficulty {
 export const fechQuizQuestions=async(amount:number ,difficulty:Difficulty)=>{
     const endpoint = `http://opentdb.com/api.php? amount=${amount}&difficulty=${difficulty}&type=multiple`
     const data =await (await fetch(endpoint)).json()
-    console.log(data);
+    return data.results.map((question:Question)=>({
+        ...question,
+        answers:shuffleArray([...question.incorrect_answers ,question.correct_answers ])
+
+    }))
+ 
     
 }
