e2c266ca5dfdd0910a743f54a16f130662f69662
pourya ghazizadeh <pourya.gh095@gmail.com>
2021-10-08 20:46:25 +0200

    fix bug

--- src/API.ts
+++ src/API.ts
@@ -3,11 +3,12 @@
 
 import { shuffleArray } from "./utils"
 export type Question={
-    category:string;
-    correct_answers:string;
-    incorrect_answers:string[];
-    question:string;
-    type:string 
+    category: string;
+    correct_answer: string;
+    difficulty: string;
+    incorrect_answers: string[];
+    question: string;
+    type: string;
 
 
 }
@@ -25,7 +26,7 @@ export const fechQuizQuestions=async(amount:number ,difficulty:Difficulty)=>{
     const data =await (await fetch(endpoint)).json()
     return data.results.map((question:Question)=>({
         ...question,
-        answers:shuffleArray([...question.incorrect_answers ,question.correct_answers ])
+        answers:shuffleArray([...question.incorrect_answers ,question.correct_answer])
 
     }))
  
